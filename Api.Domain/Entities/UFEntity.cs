3f4d818a7666dd21ddfccaa9a55baab13f07b0f5
Lucas Rosa <lucasdarosasd@gmail.com>
2022-06-19 22:30:29 -0300

    feat - implementação das entidades de endereço

---
+++ Api.Domain/Entities/UFEntity.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Domain.Entities
+{
+    public class UFEntity : BaseEntity
+    {
+        [Required]
+        [MaxLength(2)]
+        public string Sigla { get; set; }
+        [Required]
+        [MaxLength(45)]
+        public string Nome { get; set; }
+        public IEnumerable<MunicipioEntity> Municipios {get;set;}
+    }
+}
